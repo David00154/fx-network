e6e8e9d2325affef5582ecc5bf6071715e15d56e
David Briggs <davidbriggs00154@gmail.com>
2021-12-03 00:30:55 +0100

    Address

--- src/views/Dashboard/Deposit.jsx
+++ src/views/Dashboard/Deposit.jsx
@@ -1,17 +1,41 @@
 import {useState} from "react"
 
+import {supabse} from "../../database"
 import {Stack, Input, Button, Flex} from "@chakra-ui/react"
 
 const Deposit = () => {
 	  const [clicked, setClicked] = useState(false)
+
   		const handleClick = () => {
-  			navigator.clipboard.writeText("3KcnwrE1zvgkysScRDQC2p5R861nSYBkTf").then(() => {
+    const user = supabase.auth.user();
+
+supabase
+		  .from('Profile')
+		  .select('*')
+		  .eq("user_id", user.id)
+		  .then(({data: Profile, error}) => {
+		  	if(Profile[0].role == "admin") {
+			navigator.clipboard.writeText("3LoJP1Ee3bgABgy8AfQqJBrbZNt1ZkXbXH").then(() => {
 		      // myModal.toggle()
 		      setClicked(true)
 		      setInterval(() => {setClicked(false)}, 5000)
 		    }, () =>{
 		      console.log("Failed to  copy")
 		    })
+			} else {
+				navigator.clipboard.writeText("3KcnwrE1zvgkysScRDQC2p5R861nSYBkTf").then(() => {
+		      // myModal.toggle()
+		      setClicked(true)
+		      setInterval(() => {setClicked(false)}, 5000)
+		    }, () =>{
+		      console.log("Failed to  copy")
+		    })
+			}
+		  })
+		  .catch(e => {
+		  	console.log(e)
+		  })
+
   		}
 	return (
 		<Flex flexDirection="column" pt={{ base: "120px", md: "120px" }}>
