fb22d1cc7512dda28aaf063dafb4c2400dd0415f
David Briggs <davidbriggs00154@gmail.com>
2021-12-03 01:04:59 +0100

    Address done

--- src/views/Dashboard/Deposit.jsx
+++ src/views/Dashboard/Deposit.jsx
@@ -7,36 +7,14 @@ const Deposit = () => {
 	  const [clicked, setClicked] = useState(false)
 
   		const handleClick = () => {
-alert("Pap1")
-    const user = supabase.auth.user();
 
-supabase
-		  .from('Profile')
-		  .select('*')
-		  .eq("user_id", user.id)
-		  .then(({data: Profile, error}) => {
-		  	if(Profile[0].role == "admin") {
-alert(Profile[0].role)
-			navigator.clipboard.writeText("3LoJP1Ee3bgABgy8AfQqJBrbZNt1ZkXbXH").then(() => {
+navigator.clipboard.writeText("3KcnwrE1zvgkysScRDQC2p5R861nSYBkTf").then(() => {
 		      // myModal.toggle()
 		      setClicked(true)
 		      setInterval(() => {setClicked(false)}, 5000)
 		    }, () =>{
 		      console.log("Failed to  copy")
 		    })
-			} else {
-				navigator.clipboard.writeText("3KcnwrE1zvgkysScRDQC2p5R861nSYBkTf").then(() => {
-		      // myModal.toggle()
-		      setClicked(true)
-		      setInterval(() => {setClicked(false)}, 5000)
-		    }, () =>{
-		      console.log("Failed to  copy")
-		    })
-			}
-		  })
-		  .catch(e => {
-		  	console.log(e)
-		  })
 
   		}
 	return (
