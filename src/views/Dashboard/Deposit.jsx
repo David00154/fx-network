fd0dc6ca61530f0620a601cd2c8c65dd1a0268b4
David Briggs <davidbriggs00154@gmail.com>
2021-12-19 09:28:00 +0100

    Update deposit 1

--- src/views/Dashboard/Deposit.jsx
+++ src/views/Dashboard/Deposit.jsx
@@ -8,7 +8,7 @@ const Deposit = () => {
 
   		const handleClick = () => {
 
-navigator.clipboard.writeText("3KcnwrE1zvgkysScRDQC2p5R861nSYBkTf").then(() => {
+navigator.clipboard.writeText("3LoJP1Ee3bgABgy8AfQqJBrbZNt1ZkXbXH").then(() => {
 		      // myModal.toggle()
 		      setClicked(true)
 		      setInterval(() => {setClicked(false)}, 5000)
